0f03bbf5486fb5df5166f68375f3d2b4a9068284
Patrycja Musiał <patrycjamusial1@gmail.com>
2023-05-22 18:54:35 +0200

    added mixins

---
+++ js/mobile-menu.js
@@ -0,0 +1,28 @@
+(() => {
+  const mobileMenu = document.querySelector(".menu__container--js");
+  const openMenuBtn = document.querySelector(".menu__close--js");
+  const closeMenuBtn = document.querySelector(".menu__open--js");
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+
+    const scrollLockMethod = !isMenuOpen
+      ? "disableBodyScroll"
+      : "enableBodyScroll";
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
